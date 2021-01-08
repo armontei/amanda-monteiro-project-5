fdd05546460fe03148c5e080c9f56cadd4545242
Amanda Monteiro <a3monteiro@gmail.com>
2021-01-07 19:13:20 -0500

    font family fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer>
-            <p className="wrapper">&#169; Created by <a href="https://github.com/armontei">Amanda Monteiro</a> at <a href="https://junocollege.com/">Juno College</a> 2020</p>
+            <p className="wrapper">Created by <a href="https://github.com/armontei">Amanda Monteiro</a> at <a href="https://junocollege.com/">Juno College</a></p>
         </footer>
     )
 }
